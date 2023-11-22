b4ea6b136f4d7d5e510cd5846b00846cb68fa66b
Raahim Ahmed <142586661+Raahim10@users.noreply.github.com>
2023-11-23 00:29:02 +0500

    update

---
+++ app.js
@@ -0,0 +1,60 @@
+
+if (window.innerWidth > 1000) {
+    gsap.to('.video-box', {
+        // width: 320,
+        width: 1200,
+        // height: "30%",
+        duration: 2,
+        delay: 2,
+        borderRadius: '500',
+        scrollTrigger: {
+            scroller: 'body',
+            trigger: '.video-box',
+            scrub: 2,
+            start: 'top 70%',
+            end: 'top 20%'
+        }
+
+
+
+    })
+}
+
+if (window.innerWidth < 767) {
+    gsap.to('.video-box', {
+        width: 340,
+        // width: 1200,
+        height: "80%",
+        duration: 2,
+        delay: 2,
+        borderRadius: '500',
+        scrollTrigger: {
+            scroller: 'body',
+            trigger: '.video-box',
+            scrub: 2,
+            start: 'top 100%',
+            end: 'top 30%'
+        }
+
+
+
+    })
+}
+
+
+
+gsap.to(".anim", {
+    x: 0,
+    duration: 1,
+    opacity: 1,
+    stagger: 0.1,
+    scrollTrigger: ".anim"
+})
+gsap.to('.anim2', {
+    y: 0,
+    duration: 1,
+    opacity: 1,
+
+    stagger: 0.1,
+    scrollTrigger: ".anim"
+})
--- script.js
+++ script.js
@@ -1,99 +1,106 @@
-function video() {
+// function video() {
 
 
 
-    window.addEventListener("scroll", () => {
-        const scrollPosition = window.scrollY;
-        const scrollableContainer = document.querySelector(".scrollable-container");
-        const scrollableImage = document.querySelector(".scrollable-image");
+//     window.addEventListener("scroll", () => {
+//         const scrollPosition = window.scrollY;
+//         const scrollableContainer = document.querySelector(".scrollable-container");
+//         const scrollableImage = document.querySelector(".scrollable-image");
 
-        if (scrollPosition >= 500) {
-            if (scrollableContainer.clientWidth !== 40) {
-                scrollableContainer.style.width = "95%";
-                scrollableImage.style.width = "130%";
-                scrollableImage.style.height = "42vw";
-                scrollableContainer.style.height = "42vw";
-                scrollableImage.style.borderRadius = '300px';
-                scrollableContainer.style.borderRadius = '300px';
-            }
-        } else {
-            if (scrollPosition <= 400) {
-                if (scrollableContainer.clientWidth !== 80) {
-                    scrollableContainer.style.width = "40%";
-                    scrollableImage.style.width = "130%";
-                    scrollableImage.style.height = "22vw";
-                    scrollableContainer.style.height = "22vw";
-                    scrollableImage.style.borderRadius = '500px';
-                    scrollableContainer.style.borderRadius = '500px';
-                }
-            }
-        }
+//         if (scrollPosition >= 500) {
+//             if (scrollableContainer.clientWidth !== 40) {
+//                 scrollableContainer.style.width = "95%";
+//                 scrollableImage.style.width = "130%";
+//                 scrollableImage.style.height = "42vw";
+//                 scrollableContainer.style.height = "42vw";
+//                 scrollableImage.style.borderRadius = '300px';
+//                 scrollableContainer.style.borderRadius = '300px';
+//             }
+//         } else {
+//             if (scrollPosition <= 400) {
+//                 if (scrollableContainer.clientWidth !== 80) {
+//                     scrollableContainer.style.width = "40%";
+//                     scrollableImage.style.width = "130%";
+//                     scrollableImage.style.height = "22vw";
+//                     scrollableContainer.style.height = "22vw";
+//                     scrollableImage.style.borderRadius = '500px';
+//                     scrollableContainer.style.borderRadius = '500px';
+//                 }
+//             }
+//         }
 
-    });
-}
-function followImage() {
-    document.addEventListener('DOMContentLoaded', () => {
-        const imageContainer = document.querySelector('.follow-image-container');
-        const img = document.querySelector('.my-follow-image');
-        let isFollowingMouse = false;
+//     });
+// }
+// function followImage() {
+//     document.addEventListener('DOMContentLoaded', () => {
+//         const imageContainer = document.querySelector('.follow-image-container');
+//         const img = document.querySelector('.my-follow-image');
+//         let isFollowingMouse = false;
 
-        // Function to handle scrolling
-        function handleScroll() {
-            const scrollY = window.scrollY;
+//         // Function to handle scrolling
+//         function handleScroll() {
+//             const scrollY = window.scrollY;
 
-            if (scrollY >= 2250) {
-                if (!isFollowingMouse) {
-                    isFollowingMouse = true;
-                    img.style.width = '25%';
-                    img.style.height = '50%';
-                    img.style.zIndex = '-1';
-                    img.style.borderRadius = '500%';
-                }
-            } else {
-                if (isFollowingMouse) {
-                    isFollowingMouse = false;
-                    img.style.width = '80%';
-                    img.style.height = '100%';
-                    img.style.borderRadius = '300px';
-                }
-            }
-        }
+//             if (scrollY >= 2250) {
+//                 if (!isFollowingMouse) {
+//                     isFollowingMouse = true;
+//                     img.style.width = '30%';
+//                     img.style.height = '60%';
+//                     img.style.zIndex = '-1';
+//                     img.style.borderRadius = '200px';
+//                     img.classList.add("oncircle")
+//                 }
+//             } else {
+//                 if (isFollowingMouse) {
+//                     isFollowingMouse = false;
+//                     // img.style.position = "relative"
+//                     img.classList.remove("oncircle")
+//                     img.style.width = '80%';
+//                     img.style.height = '60%';
+//                     img.style.borderRadius = '200px';
+//                 }
+//             }
+//         }
 
-        // Function to handle mouse movement
-        function handleMouseMove(e) {
-            if (isFollowingMouse) {
-                const mouseX = e.clientX;
-                const mouseY = e.clientY;
-                const containerRect = imageContainer.getBoundingClientRect();
-                const containerX = containerRect.left + containerRect.width / 2;
-                const containerY = containerRect.top + containerRect.height / 2;
-                const translateX = mouseX - containerX;
-                const translateY = mouseY - containerY;
-                img.style.transform = `translate(${translateX}px, ${translateY}px)`;
-            }
-        }
+//         // Function to handle mouse movement
+//         function handleMouseMove(e) {
+//             if (isFollowingMouse) {
+//                 // const mouseX = e.clientX;
+//                 // const mouseY = e.clientY;
+//                 // const containerRect = imageContainer.getBoundingClientRect();
+//                 // const containerX = containerRect.left + containerRect.width / 2;
+//                 // const containerY = containerRect.top + containerRect.height / 2;
+//                 // const translateX = mouseX - containerX;
+//                 // const translateY = mouseY - containerY;
+//                 var x = e.clientX;
+//                 var y = e.clientY;
+//                 img.style.left = x + 'px';
+//                 img.style.top = y + 'px';
+//                 // img.style.transform = `translate(${translateX}px, ${translateY}px)`;
+//             }
+//         }
 
-        // Function to handle scrolling end and return image to original location
-        let scrollTimeout;
-        function handleScrollEnd() {
-            clearTimeout(scrollTimeout);
-            scrollTimeout = setTimeout(() => {
-                if (window.scrollY <= 2200) {
-                    img.style.transform = 'translate(0, 0)';
-                    // img.classlist.add("my-image")
-                    img.style.width = "80% !important"
-                    img.style.transition = "width 0.5s ease-in-out";
+//         // Function to handle scrolling end and return image to original location
+//         let scrollTimeout;
+//         function handleScrollEnd() {
+//             clearTimeout(scrollTimeout);
+//             scrollTimeout = setTimeout(() => {
+//                 if (window.scrollY <= 2200) {
+//                     img.style.transform = 'translate(0, 0)';
+//                     // img.classlist.add("my-image")
+//                     img.style.width = "80% !important"
+//                     img.style.transition = "width 0.5s ease-in-out";
 
-                }
-            }, 10); // Adjust the delay as needed
-        }
-        console.log(window.scrollYs)
-        // Event listeners
-        window.addEventListener('scroll', handleScroll);
-        window.addEventListener('mousemove', handleMouseMove);
-        window.addEventListener('scroll', handleScrollEnd);
-    });
-}
+//                 }
+//             }, 10); // Adjust the delay as needed
+//         }
+//         console.log(window.scrollYs)
+//         // Event listeners
+//         window.addEventListener('scroll', handleScroll);
+//         window.addEventListener('mousemove', handleMouseMove);
+//         window.addEventListener('scroll', handleScrollEnd);
+//     });
+// }
 
 
 function mobileMenu() {
@@ -113,8 +120,32 @@ function mobileMenu() {
 
 }
 
+function onScroll() {
+    let sections = document.querySelectorAll('.myouter');
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 700;
+            let height = sec.offsetHeight;
+            if (top >= offset) {
+                sec.classList.add('show-animate');
+                offset = sec.offsetTop + 100;
+            }
+
+            // if want to use repeating animation on scroll, use this
+            else {
+                sec.classList.remove('show-animate');
+
+            }
+
+        })
+    }
+}
 
 
-video();
-followImage();
+
+
+
+
 mobileMenu();
+onScroll()
